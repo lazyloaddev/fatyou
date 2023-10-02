fbbd58744779b4508cbbe61475e00b02d5db0d1b
Evgenii Elchev <yolchev@gmail.com>
2023-10-02 17:46:03 +0700

    fatyou5

--- FatYou/App/UI/Screens/FoodDiaryScreen.swift
+++ FatYou/App/UI/Screens/FoodDiaryScreen.swift
@@ -51,25 +51,82 @@ struct FoodDiaryScreen: View {
 
 private struct CaloryInfo: View {
 
+    @State
+    private var isCollapsed = false
+
     var body: some View {
-        HStack(spacing: 0) {
-            Button  {
+        VStack(spacing: 0) {
+            HStack(spacing: 0) {
+                Button {
+                    withAnimation {
+                        isCollapsed.toggle()
+                    }
+                } label: {
+                    HStack(alignment: .bottom, spacing: 11) {
+                        Text("235 cal left")
+                            .customFont(.h2)
+                            .lineLimit(1)
+                        Image(.upSmall)
+                            .rotationEffect(isCollapsed ? .zero : .degrees(180))
+                    }.foregroundColor(Color(.mainText))
+                }.frame(minWidth: 100)
 
-            } label: {
-                Text("123")
-            }
-            Spacer()
-            Button  {
+                Spacer()
 
-            } label: {
-                Text("123")
+                Button  {
+
+                } label: {
+                    Text("Add Food")
+                        .customFont(.smallButtonTxt)
+                        .lineLimit(1)
+                        .foregroundColor(Color(.white))
+                        .padding(EdgeInsets(top: 8, leading: 14, bottom: 8, trailing: 14))
+                        .background(Color(.accent))
+                        .clipShape(RoundedCorner(radius: 10))
+                }.layoutPriority(1)
             }
+            // jdhsjfkd
+
+            PacmanProgress(percent: 0.5)
+                .padding(.top, 16)
+                .frame(maxHeight: isCollapsed ? 0 : .infinity)
+                .opacity(isCollapsed ? 0.0 : 1.0)
+                .clipped()
         }
-        .padding(EdgeInsets(top: 20, leading: 17, bottom: 23, trailing: 17))
+        .padding(EdgeInsets(top: 20, leading: 20, bottom: 23, trailing: 17))
         .background(Color(.white))
         .clipShape(RoundedCorner(radius: 20, corners: [.topLeft, .topRight]))
     }
 
+}
+
+private struct PacmanProgress: View {
+
+    let percent: Double
+
+    var body: some View {
+        let headSize = 60.0
+        let dotSize = 14.0
+        let dotSpace = 5.0
+
+        GeometryReader { proxy in
+            let minSize = headSize / 2
+            let actualSize = (proxy.size.width - minSize) * percent
+            let dotCount = Int(actualSize / (dotSize + dotSpace))
+            ZStack(alignment: .leading) {
+                HStack(spacing: dotSpace) {
+                    ForEach(0...dotCount, id: \.self) { _ in
+                        Circle()
+                            .fill(Color(.lightBlue))
+                            .frame(width: dotSize, height: dotSize)
+                    }
+                }.padding(.leading, headSize / 2)
+                Image(.packmanHead)
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+        .frame(height: headSize)
+    }
 }
 
 struct FoodDiaryScreen_Previews: PreviewProvider {
